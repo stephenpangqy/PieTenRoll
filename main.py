1f62b08cbad71ad0e3664fe1af39a57b111c4a89
Hazel <hazelma.2020@smu.edu.sg>
2022-01-07 16:03:08 +0800

    added new code to main,py

--- main.py
+++ main.py
@@ -71,102 +71,89 @@ def start(message):
     Command that welcomes the user and configures the initial setup
     """
     chat_id = message.chat.id
-    id_Exists = idExists(chat_id)
-    if id_Exists:
-        username = Users.query.filter_by(chat_id=chat_id).first().name
-        bot.send_sticker(
-            chat_id=chat_id, 
-            data='CAACAgUAAxkBAAEDoRJh1y0KgigTU87x7QYrbKJNbfDavQACawMAAlobywF60Koi6G4EECME'
-        )
-
-        buttons = [
-            InlineKeyboardButton(
-            text = "Find groupmates",
-            callback_data = "Find_groupmates"
-        ),
-            InlineKeyboardButton(
-            text = "Find group",
-            callback_data = "Find_group"
-        )]
-        keyboard = InlineKeyboardMarkup()
-        for button in buttons:
-            keyboard.add(button)
-        message_text = f'Welcome back {username}, Please select if you are finding a group member or looking for a group.'
-        bot.send_message(chat_id, message_text, reply_markup = keyboard)
+    if message.chat.type == 'private':
+        chat_user = message.chat.first_name
     else:
-        bot.send_message(chat_id, f'Welcome to GroupTogether bot, We help you find your teammates with ease!')
-        msg = bot.send_message(message.chat.id,"It looks like this is your first time using me. Please enter your name in the next chat bubble; this will be the name that others see you as.")
-        bot.register_next_step_handler(msg, register)
+        chat_user = message.chat.title
+    
+    bot.send_sticker(
+        chat_id=chat_id, 
+        data='CAACAgUAAxkBAAEDoRJh1y0KgigTU87x7QYrbKJNbfDavQACawMAAlobywF60Koi6G4EECME'
+    )
 
-def register(message):
-    chat_id = message.chat.id
-    name = message.text.strip()
-    if name == "":
-        msg = bot.reply_to(message,'Your name cannot be empty. Please enter your name again!')
-        bot.register_next_step_handler(msg,register)
-        return
-    elif len(name) > 100:
-        msg = bot.reply_to(message,"Your name cannot be longer than 100 characters. Please enter your name again!")
-        bot.register_next_step_handler(msg,register)
-        return
-    else:
-        new_user = Users(chat_id=chat_id,name=name)
-        db.session.add(new_user)
-        db.session.commit()
-        bot.reply_to(message,"Thank you, " + name + ", you have successfully registered.")
-        
-        bot.send_sticker(
-            chat_id=chat_id, 
-            data='CAACAgUAAxkBAAEDoRJh1y0KgigTU87x7QYrbKJNbfDavQACawMAAlobywF60Koi6G4EECME'
-        )
-
-        buttons = [
-            InlineKeyboardButton(
-            text = "Find groupmates",
-            callback_data = "Find_groupmates"
-        ),
-            InlineKeyboardButton(
-            text = "Find group",
-            callback_data = "Find_group"
-        )]
-        keyboard = InlineKeyboardMarkup()
-        for button in buttons:
-            keyboard.add(button)
-        message_text = f'Now, Please select if you are finding a group member or looking for a group.'
-        bot.send_message(chat_id, message_text, reply_markup = keyboard)
+    buttons = [
+        InlineKeyboardButton(
+        text = "Find groupmates",
+        callback_data = "Find_groupmates"
+    ),
+        InlineKeyboardButton(
+        text = "Find group",
+        callback_data = "Find_group"
+    )]
+    keyboard = InlineKeyboardMarkup()
+    for button in buttons:
+        keyboard.add(button)
+    message_text = f'Welcome back {chat_user}, Please select if you are finding a group member or looking for a group.'
+    bot.send_message(chat_id, message_text, reply_markup = keyboard)
 
 @bot.callback_query_handler(lambda query: query.data == 'Find_groupmates')
 def handle_callback(call):
-  """
-  Handles the execution of the respective functions upon receipt of the callback query
-  """
-  chat_id = call.message.chat.id
+    """
+    Handles the execution of the respective functions upon receipt of the callback query
+    """
+    chat_id = call.message.chat.id
 
-  #bot.register_next_step_handler(msg,confirmEvent)
-  pass
+    #bot.register_next_step_handler(msg,confirmEvent)
+    pass
 
 @bot.callback_query_handler(lambda query: query.data == 'Find_group')
 def handle_callback(call):
-  """
-  Handles the execution of the respective functions upon receipt of the callback query
-  """
-  chat_id = call.message.chat.id
-  
-  pass
-
-def enter_school(chat_id):
-    bot.send_message(chat_id, "Please type in your school name (Eg: NUS)")
-
-def enter_module(chat_id):
-    bot.send_message(chat_id, "Please type in your module code")
+    """
+    Handles the execution of the respective functions upon receipt of the callback query
+    """
+    chat_id = call.message.chat.id
+    msg = bot.send_message(chat_id, "Please type in your school name (Eg: NUS)")
+    bot.register_next_step_handler(msg, enter_school)
     
-def enter_section(chat_id):
-    bot.send_message(chat_id, "Please type in your section")
+    
 
-def enter_semester(chat_id):
-    bot.send_message(chat_id, "Please type in your semester")
+def enter_school(message):
+    chat_id = message.chat.id
+    school = message.text.upper().strip()
+    school_list = ['NUS','NTU','SMU']
+    if school not in school_list: 
+        msg = bot.reply_to(message,'School is invalid, please try again!')
+        bot.register_next_step_handler(msg,enter_school)
+        return
+    else:
+        # new_record = Looking_For_Group(chat_id=chat_id,school=school)
+        # db.session.add(new_record)
+        # db.session.commit()
+        msg = bot.send_message(chat_id, "Please type in your module code (Eg: IS216)")
+        bot.register_next_step_handler(msg, enter_module)
 
-def enter_avail(chat_id):
-    bot.send_message(chat_id, "Please type in the number of available slots")
+def enter_module(message):
+    chat_id = message.chat.id
+    module = message.text.strip
+    msg = bot.send_message(chat_id, "Please type in your section (Eg: G11)")
+    bot.register_next_step_handler(msg, enter_section)
+
+    
+def enter_section(message):
+    chat_id = message.chat.id
+    section = message.text.strip
+    msg = bot.send_message(chat_id, "Please type in your semester (1 or 2)")
+    bot.register_next_step_handler(msg, enter_semester)
+
+
+def enter_semester(message):
+    chat_id = message.chat.id
+    semester = message.text.strip
+    
+    
+
+def enter_avail(message):
+    chat_id = message.chat.id
+    
     
 bot.infinity_polling()
