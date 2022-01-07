8b46aee3cb58045e5c148a557c276c2f369cb7e3
Darien <63910112+Darien2805@users.noreply.github.com>
2022-01-07 10:43:01 +0800

    Beginning of main.py
    Importing of libraries, api key and bot command

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+
+import telebot
+from telebot.types import BotCommand, InlineKeyboardButton, InlineKeyboardMarkup, LabeledPrice
+
+API_KEY = "5047659649:AAHxljzEetaON7tXSqaCiFbNXckHFoHnIrg"
+bot = telebot.TeleBot(API_KEY)
+
+bot.set_my_commands([
+    BotCommand('start', 'Start finding your groupmates now!'),
+])
