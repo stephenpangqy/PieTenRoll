9fbd2894493e2e56a064d0a04fcb657f0aa3568d
Stephen Pang <61278650+stephenpangqy@users.noreply.github.com>
2022-01-07 11:05:38 +0800

    Added SQL file

---
+++ load.sql
@@ -0,0 +1,38 @@
+drop database if exists grouptogetherDB;
+create database grouptogetherDB;
+use grouptogetherDB;
+
+create table users (
+	chat_id int not null primary key,
+    name varchar(255) not null
+);
+
+create table looking_for_group (
+	chat_id int NOT NULL,
+    school varchar(100) NOT NULL,
+    module_code varchar(100) NOT NULL,
+    semester int NOT NULL,
+    
+    constraint primary key (chat_id,school,module_code,semester)
+);
+
+create table looking_for_members (
+	chat_id int NOT NULL,
+    school varchar(100) NOT NULL,
+    module_code varchar(100) NOT NULL,
+    semester int NOT NULL,
+    num_members_need int NOT NULL,
+    
+    constraint primary key (chat_id,school,module_code,semester)
+);
+
+create table match_found (
+	finder_chat_id int NOT NULL,
+    looker_chat_id int NOT NULL,
+    school varchar(100) NOT NULL,
+    module_code varchar(100) NOT NULL,
+    semester int NOT NULL,
+    accepted char(1) NOT NULL, # P- Pending, A - Accepted, R - rejected
+    
+    constraint primary key (finder_chat_id,looker_chat_id,school,module_code,semester)
+);
